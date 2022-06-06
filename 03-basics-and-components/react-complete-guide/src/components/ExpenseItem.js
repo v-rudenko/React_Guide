bda6c3d426a745ac6a663126424d53c4285f43e2
Viktor Rudenko <v.rudenko@triacom.ua>
2022-06-06 17:22:01 +0300

    before hometask

--- 03-basics-and-components/react-complete-guide/src/components/ExpenseItem.js
+++ 03-basics-and-components/react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-  console.log(expenseDate);
+function ExpenseItem(props) {
+
 
   return (
     <div className="expense-item">
-      <h2>{expenseDate.toISOString()}</h2>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
