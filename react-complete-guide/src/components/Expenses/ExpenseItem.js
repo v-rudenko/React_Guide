adf5b3f5b9c7842eba14d143e22ddbd42013fce2
Viktor Rudenko <v.rudenko@triacom.ua>
2022-07-10 23:23:12 +0300

    Section 4 completed

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,34 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  //function clickHandler
-  const [title, setTitle] = useState(props.title);
   console.log("ExpenseItem evaluated by React");
 
-  // let title = props.title
-
-  const clickHandler = () => {
-    // setTimeout(() => {
-    //   setTitle('Оновлена назва');
-    // }, 3000)
-
-    setTimeout(setTitle("Оновлена назва"), 3000);
-
-    // setTitle('Updated!!!')
-    console.log(`Натиснуто кнопку: ${title}`);
-  };
+    // setTimeout(setTitle("Оновлена назва"), 3000);
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">₴{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Змінити назву</button>
     </Card>
   );
 };
