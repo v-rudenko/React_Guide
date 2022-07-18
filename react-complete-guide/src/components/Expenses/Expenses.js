9711b514b376e527c175dfdcf7759b7f0d9b5745
Viktor Rudenko <v.rudenko@triacom.ua>
2022-07-19 00:44:54 +0300

    assigment 3 completed

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -2,7 +2,6 @@
 // Expenses.js
 ////////////////////////////////////////////////
 
-
 import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
@@ -10,43 +9,60 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const expenses = props.items;
+  const defaultYear = "2021";
 
-  const [selectedYear, setSelectedYear] = useState('2020')
+  const [selectedYear, setSelectedYear] = useState(defaultYear);
+
+  // Страшна, довга умова. Треба придумати щось краще.
+
+  const [expensesFilteredByYear, setExpensesFilteredByYear] = useState(
+    props.items.filter(
+      (expense) => expense.date.getFullYear().toString() === defaultYear
+    )
+  );
 
   const filterSavedHandler = (year) => {
     setSelectedYear(year);
-    // console.log(selectedYear);
+    setExpensesFilteredByYear(
+      props.items.filter(
+        (expense) => expense.date.getFullYear().toString() === year
+      )
+    );
   };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={selectedYear} onFilterSaved={filterSavedHandler}/>
+        <ExpensesFilter
+          selected={selectedYear}
+          onFilterSaved={filterSavedHandler}
+        />
+        {/* {expensesFilteredByYear.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))} */}
+
+
+        {/* Це - жахіття */}
+        {props.items
+          .filter((expense) => expense.date.getFullYear().toString() === selectedYear)
+          .map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+
         <h1>Selected year: {selectedYear}</h1>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        ></ExpenseItem>
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
