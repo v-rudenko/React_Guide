c5ad4ab8ec7932e252a4a42ff731de8756a1f865
Viktor Rudenko <v.rudenko@triacom.ua>
2022-10-19 12:57:50 +0300

    272

---
+++ 20-react-router/router-project/src/pages/Products.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
