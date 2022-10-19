0aba234f8f9b1a8cfeff03aa7a58f7b2a5cb6619
Viktor Rudenko <v.rudenko@triacom.ua>
2022-10-19 13:20:26 +0300

    273

--- 20-react-router/router-project/src/pages/Products.js
+++ 20-react-router/router-project/src/pages/Products.js
@@ -5,9 +5,9 @@ const Products = () => {
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An online course</li>
       </ul>
     </section>
   );
