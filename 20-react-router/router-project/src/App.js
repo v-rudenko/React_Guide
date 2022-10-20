aa90d85cdc27054d820dc72b5cbbbf801cc1840a
Viktor Rudenko <v.rudenko@triacom.ua>
2022-10-20 14:52:49 +0300

    react router 277

--- 20-react-router/router-project/src/App.js
+++ 20-react-router/router-project/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -7,17 +7,22 @@ import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <div>
-      <MainHeader/>
+      <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path='/product-detail/:productId'>
-          <ProductDetail></ProductDetail>
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"></Redirect>
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail></ProductDetail>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- 20-react-router/router-project/src/pages/Welcome.js
+++ 20-react-router/router-project/src/pages/Welcome.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
   return (
-    <h1>The Welcome Page</h1>
-  )
-}
+    <section>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
+    </section>
+  );
+};
 
-export default Welcome
+export default Welcome;
