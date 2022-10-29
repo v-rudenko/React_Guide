ed7f29ab650a2d675911e835e69bea5ee10a502c
Viktor Rudenko <v.rudenko@triacom.ua>
2022-10-29 18:53:26 +0300

    242

--- 18-redux/redux-starting-project/src/store/index.js
+++ 18-redux/redux-starting-project/src/store/index.js
@@ -8,6 +8,25 @@ export const INCREMENT = 'increment'
 
 const initialState = { counter: 0, showCounter: true };
 
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
+    },
+  }
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === INCREMENT) {
     return {
