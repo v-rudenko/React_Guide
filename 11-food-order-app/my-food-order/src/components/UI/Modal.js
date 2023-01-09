a02748fafe88edcb4c637dbe7e882afc952513e7
Viktor Rudenko <v.rudenko@triacom.ua>
2023-01-09 14:33:08 +0200

    140-Managing-Cart-and-Modal-State

--- 11-food-order-app/my-food-order/src/components/UI/Modal.js
+++ 11-food-order-app/my-food-order/src/components/UI/Modal.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
-const Modal = () => {
+import classes from "./Modal.module.scss";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <div>Modal</div>
-  )
-}
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
 
-export default Modal
+const portalElement = document.getElementById('overlays')
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </Fragment>
+  );
+};
+
+export default Modal;
