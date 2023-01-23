b15a7ad1dacad14766d31f63f5bbaaf52f42e072
Viktor Rudenko <v.rudenko@triacom.ua>
2023-01-23 15:01:23 +0200

    147-making-items-removable

--- 11-food-order-app/my-food-order/src/store/CartProvider.js
+++ 11-food-order-app/my-food-order/src/store/CartProvider.js
@@ -9,13 +9,32 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-      return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    ); // Шукати індекс елемента в масиві
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
       };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem
+
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
