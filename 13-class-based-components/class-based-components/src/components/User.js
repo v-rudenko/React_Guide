529ee3969d284d8e85a14e73aee4949f13e3b534
Viktor Rudenko <v.rudenko@triacom.ua>
2023-02-08 12:26:16 +0200

    168

--- 13-class-based-components/class-based-components/src/components/User.js
+++ 13-class-based-components/class-based-components/src/components/User.js
@@ -4,6 +4,9 @@ import classes from './User.module.css';
 
 
 class User extends Component  {
+  componentWillUnmount() {
+    console.log("User will unmount")
+  }
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
