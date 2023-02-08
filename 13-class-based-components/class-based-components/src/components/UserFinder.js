70b70d1849de0225efa036304eba3b3927ae6a2d
Viktor Rudenko <v.rudenko@triacom.ua>
2023-02-08 12:59:49 +0200

    170

--- 13-class-based-components/class-based-components/src/components/UserFinder.js
+++ 13-class-based-components/class-based-components/src/components/UserFinder.js
@@ -3,31 +3,29 @@ import { Fragment, useState, useEffect, Component } from "react";
 import Users from "./Users";
 
 import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
   }
 
   componentDidMount() {
     // Send http request
-    this.setState({filteredUsers: DUMMY_USERS})
+    this.setState({filteredUsers: this.context.users})
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
