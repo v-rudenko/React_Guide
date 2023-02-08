4312599e1b0912f740d69ee0a6e375e85879cecf
Viktor Rudenko <v.rudenko@triacom.ua>
2023-02-08 13:25:14 +0200

    section 13 completed

--- 13-class-based-components/class-based-components/src/components/UserFinder.js
+++ 13-class-based-components/class-based-components/src/components/UserFinder.js
@@ -4,6 +4,7 @@ import Users from "./Users";
 
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   static contextType = UsersContext;
@@ -41,7 +42,7 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary><Users users={this.state.filteredUsers} /></ErrorBoundary>
       </Fragment>
     );
   }
