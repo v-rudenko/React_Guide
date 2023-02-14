569fa247eb60f3fd16f9c83be3d4de1c49d9a7a8
Viktor Rudenko <v.rudenko@triacom.ua>
2023-02-14 13:38:27 +0200

    180. Using useEffect() For Requests

--- 14-sending-http-requests/sending-http-project/src/App.js
+++ 14-sending-http-requests/sending-http-project/src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
+    setError(null);
     // fetch("https://swapi.dev/api/films/")
     //   .then((response) => {
     //     return response.json();
@@ -27,20 +29,43 @@ function App() {
     //   });
 
     // const response = await (await fetch("https://swapi.dev/api/films/")).json()
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+      const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies</p>;
+  if (!isLoading) {
+    if (error) {
+      content = <p>{error}</p>;
+    } else {
+      if (movies.length === 0) {
+        content = <p>Found no movies</p>;
+      }
+      if (movies.length > 0) {
+        content = <MoviesList movies={movies} />;
+      }
+    }
+  } else {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -49,9 +74,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
-        {isLoading && <p>Loading...</p>}
+      {content}
       </section>
     </React.Fragment>
   );
