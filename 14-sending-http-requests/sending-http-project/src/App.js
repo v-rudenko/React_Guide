c8836b2215cb9a9193540799902a4aedcadccda4
Viktor Rudenko <v.rudenko@triacom.ua>
2023-02-14 12:59:37 +0200

    179. Handling Http Errors

--- 14-sending-http-requests/sending-http-project/src/App.js
+++ 14-sending-http-requests/sending-http-project/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     // fetch("https://swapi.dev/api/films/")
     //   .then((response) => {
     //     return response.json();
@@ -38,6 +40,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -46,7 +49,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
