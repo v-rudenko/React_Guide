429b665d1ba8a6556e2fe56387cfcd3aadbf39ae
Viktor Rudenko <v.rudenko@triacom.ua>
2023-02-14 14:30:29 +0200

    181. Preparing for the next Steps

--- 14-sending-http-requests/sending-http-project/src/App.js
+++ 14-sending-http-requests/sending-http-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     // fetch("https://swapi.dev/api/films/")
@@ -50,7 +50,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>;
   if (!isLoading) {
@@ -73,9 +77,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-      {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
