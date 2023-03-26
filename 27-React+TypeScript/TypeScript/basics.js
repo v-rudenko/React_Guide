700b21ece3974db2eb1d9f137c9302a24529ff60
Viktor Rudenko <v-rudenko@triacom.ua>
2023-03-26 17:37:31 +0300

    424. Creating a React + Typescript Project

--- 27-React+TypeScript/TypeScript/basics.js
+++ 27-React+TypeScript/TypeScript/basics.js
@@ -1,6 +1,15 @@
 // Primitives: number, string, boolean
 // More complex types: arrays objects
 // Function types, parameters
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // Primitives
 var age;
 age = 12;
@@ -30,3 +39,14 @@ function add(a, b) {
 function Print(value) {
     console.log(value);
 }
+Print("Бабун");
+console.log("Бабун");
+// Generics
+function insertAtBeginning(array, value) {
+    var newArray = __spreadArray([value], array, true);
+    return newArray;
+}
+;
+var demoArray = [1, 2, 3];
+var updatedArray = insertAtBeginning(demoArray, -1);
+console.log(updatedArray);
--- 27-React+TypeScript/TypeScript/basics.ts
+++ 27-React+TypeScript/TypeScript/basics.ts
@@ -58,3 +58,21 @@ function Print(value:any) {
 
 Print("Бабун")
 console.log("Бабун")
+
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+};
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1);
+
+console.log(updatedArray);
+
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
+
+// updatedArray[0].split('');
