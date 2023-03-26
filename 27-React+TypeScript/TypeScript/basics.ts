2553eb4fb5a6b239395ca1c5c5d1125629cadc85
Viktor Rudenko <v-rudenko@triacom.ua>
2023-03-26 16:44:09 +0300

    419. Using Union Types

---
+++ 27-React+TypeScript/TypeScript/basics.ts
@@ -0,0 +1,49 @@
+// Primitives: number, string, boolean
+// More complex types: arrays objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Viktor";
+
+let isInstructor: boolean;
+
+isInstructor = false;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Viktor",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// Type inference
+
+let course: string | number = "React with Typescript"
+
+course = 1234;
+
