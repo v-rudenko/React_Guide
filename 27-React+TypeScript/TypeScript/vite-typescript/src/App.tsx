610ad1904965735831c2b533d4a281952c2f471b
Viktor Rudenko <v-rudenko@triacom.ua>
2023-03-28 12:46:49 +0300

    432. Managing State & TypeScript

--- 27-React+TypeScript/TypeScript/vite-typescript/src/App.tsx
+++ 27-React+TypeScript/TypeScript/vite-typescript/src/App.tsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import Todo from "./models/todo";
 import Todos from "./components/Todos";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo("Learn TypeScript"),
-  ]
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  const addTodoHandler = (todoText:string) => {
+
+  };
 
   return (
     <div className="App">
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}>Baboon</Todos>
     </div>
   );
