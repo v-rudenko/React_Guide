4a9f67a1faeb173f7188cc595cef20e40e91b883
Viktor Rudenko <darthvitek@gmail.com>
2023-04-24 16:40:39 +0300

    203. React To Lost Focus

--- 16-working-with-forms/working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/working-with-forms/src/components/SimpleInput.js
@@ -1,9 +1,18 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid!");
+    }
+  }, [enteredNameIsValid]);
+
+  // Спосіб з використанням стану
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -14,22 +23,31 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    setEnteredNameTouched(true);
+
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
     }
 
-    setEnteredNameIsValid(true)
+    setEnteredNameIsValid(true);
+
+    // Спосіб з використанням рефа
 
     const enteredValue = nameInputRef.current.value;
 
-    console.log(enteredName);
-    console.log(enteredValue);
+    console.log(`From state: ${enteredName}`);
+    console.log(`From ref: ${enteredValue}`);
 
+    // nameInputRef.current.value = "";   // Погане рішення, через використання звичайного JavaScript
     setEnteredName("");
   };
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -42,7 +60,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
-        {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
