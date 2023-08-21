33d960c4329ae8c7a3af51804d5d27efa270442e
Viktor <v.rudenko@triacom.ua>
2023-08-21 16:01:03 +0300

    21. Arrow Functions

--- 02-javascript-refresher/course-got-updated/assets/scripts/app.js
+++ 02-javascript-refresher/course-got-updated/assets/scripts/app.js
@@ -1,3 +1,11 @@
-import { apiKey } from "./util.js";
+function createGreeting(userName, message = "Hello!") {
+  // console.log(userName);
+  // console.log(message);
 
-console.log(apiKey);
+  return "Hi, I am " + userName + ". " + message;
+}
+
+const greeting = createGreeting("Viktor");
+
+console.log(greeting);
+
