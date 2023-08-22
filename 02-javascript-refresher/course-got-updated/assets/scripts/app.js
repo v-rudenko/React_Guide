7da4cdbf11448259987e4261c5fb8f5f94846afc
Viktor <v.rudenko@triacom.ua>
2023-08-22 15:39:53 +0300

    29. Manipulating the DOM - Not With React!

--- 02-javascript-refresher/course-got-updated/assets/scripts/app.js
+++ 02-javascript-refresher/course-got-updated/assets/scripts/app.js
@@ -1,11 +1,46 @@
-function createGreeting(userName, message = "Hello!") {
-  // console.log(userName);
-  // console.log(message);
+// // function createGreeting(userName, message = "Hello!") {
+// //   // console.log(userName);
+// //   // console.log(message);
 
-  return "Hi, I am " + userName + ". " + message;
-}
+// //   return "Hi, I am " + userName + ". " + message;
+// // }
 
-const greeting = createGreeting("Viktor");
+// // const greeting = createGreeting("Viktor");
 
-console.log(greeting);
+// // console.log(greeting);
 
+// const user = {
+//   name: "Віктор",
+//   age: 24,
+//   greet() {
+//     console.log("Привіт!")
+//     console.log(this.age)
+//     console.log(user.age)
+//   }
+// }
+
+// console.log(user.name);
+// user.greet();
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log("Hi!");
+//   }
+// }
+
+// const user1 = new User("Manuel", 35);
+// console.log(user1);
+// user1.greet();
+
+
+const hobbies = ["Sports", "Cooking", "Reading"];
+console.log(hobbies[0])
+
+const index = hobbies.findIndex((item) => item === "Sports");
+
+console.log(index)
