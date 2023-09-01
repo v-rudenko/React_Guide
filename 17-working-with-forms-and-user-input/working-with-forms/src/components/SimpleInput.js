0c4dc744762a1d62849976c273bfc51abbb649ae
Viktor <v.rudenko@triacom.ua>
2023-09-01 11:50:10 +0300

    241. Re-Using The Custom Hook

--- 17-working-with-forms-and-user-input/working-with-forms/src/components/SimpleInput.js
+++ 17-working-with-forms-and-user-input/working-with-forms/src/components/SimpleInput.js
@@ -1,16 +1,36 @@
 import { useState } from "react";
+import useInput from "../hooks/use-input";
 
 const SimpleInput = () => {
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
 
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    isEmpty: enteredEmailIsEmpty,
+    valueChangeHandler: emailChangedHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput(
+    (value) =>
+      value.match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      )
+  );
+
+  // const [enteredEmail, setEnteredEmail] = useState("");
   const [showEmailError, setShowEmailError] = useState(false);
-  const [emailErrorText, setEmailErrorText] = useState("");
+  // const [emailErrorText, setEmailErrorText] = useState("");
 
-  const enteredNameIsValid = enteredName.trim() !== "";
-  const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false)
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  // const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false);
 
   let formIsValid = false;
 
@@ -18,67 +38,51 @@ const SimpleInput = () => {
     formIsValid = true;
   }
 
-
-
   // Спосіб з використанням стану
 
-  const nameInputChangeHandler = (event) => {
-    setEnteredName(event.target.value);
-  };
+  // const emailInputChangeHandler = (event) => {
+  //   setEnteredEmail(event.target.value);
+  //   if (showEmailError) {
+  //     setShowEmailError(false);
+  //   }
+  // };
 
-  const emailInputChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-    if (showEmailError) {
-      setShowEmailError(false);
-    }
-  };
-
-  const nameInputBlurHandler = () => {
-    setEnteredNameTouched(true);
-  };
-
-  const emailInputBlurHandler = () => {
-
-    if (enteredEmail.trim() === "") {
-      setShowEmailError(true);
-      setEmailErrorText("Email must not be empty");
-      setEnteredEmailIsValid(false)
-    } else if (
-      !enteredEmail.match(
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-      )
-    ) {
-      setShowEmailError(true);
-      setEmailErrorText("Email is invalid");
-      setEnteredEmailIsValid(false)
-    } else {
-      setShowEmailError(false);
-      setEmailErrorText("");
-      setEnteredEmailIsValid(true)
-    }
-  };
+  // const emailInputBlurHandler = () => {
+  //   if (enteredEmail.trim() === "") {
+  //     setShowEmailError(true);
+  //     setEmailErrorText("Email must not be empty");
+  //     setEnteredEmailIsValid(false);
+  //   } else if (
+  //     !enteredEmail.match(
+  //       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  //     )
+  //   ) {
+  //     setShowEmailError(true);
+  //     setEmailErrorText("Email is invalid");
+  //     // setEnteredEmailIsValid(false);
+  //   } else {
+  //     setShowEmailError(false);
+  //     setEmailErrorText("");
+  //     // setEnteredEmailIsValid(true);
+  //   }
+  // };
+  const emailErrorText = enteredEmailIsEmpty ? "Email must not be empty" : "Email is invalid";
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    setEnteredNameTouched(true);
-
     if (!enteredNameIsValid) {
       return;
     }
-
-    setEnteredName("");
-    setEnteredEmail("")
-    setEnteredNameTouched(false);
+    resetNameInput();
+    resetEmailInput();
   };
 
-
-
-  const nameInputClasses = nameInputIsInvalid
+  const nameInputClasses = nameInputHasError
     ? "form-control invalid"
     : "form-control";
 
-  const emailInputClasses = showEmailError
+  const emailInputClasses = emailInputHasError
     ? "form-control invalid"
     : "form-control";
 
@@ -89,11 +93,11 @@ const SimpleInput = () => {
         <input
           type="text"
           id="name"
-          onChange={nameInputChangeHandler}
-          onBlur={nameInputBlurHandler}
+          onChange={nameChangedHandler}
+          onBlur={nameBlurHandler}
           value={enteredName}
         />
-        {nameInputIsInvalid && (
+        {nameInputHasError && (
           <p className="error-text">Name must not be empty.</p>
         )}
       </div>
@@ -103,11 +107,11 @@ const SimpleInput = () => {
         <input
           type="text"
           id="email"
-          onChange={emailInputChangeHandler}
-          onBlur={emailInputBlurHandler}
+          onChange={emailChangedHandler}
+          onBlur={emailBlurHandler}
           value={enteredEmail}
         />
-        {showEmailError && <p className="error-text">{emailErrorText}</p>}
+        {emailInputHasError && <p className="error-text">{emailErrorText}</p>}
       </div>
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
