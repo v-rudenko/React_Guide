70394226c55f99dd2f7ab1aacc5c07bd37ec39ef
Viktor <v.rudenko@triacom.ua>
2024-02-21 12:40:31 +0200

    test commit

--- 21-react-router/router-project/src/pages/Home.js
+++ 21-react-router/router-project/src/pages/Home.js
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
   return (
-    <div>My Home Page</div>
+    <>
+      <div>My Home Page</div>
+      <p>Go to <Link to="/products">the list of products</Link></p>
+    </>
   )
 }
 export default HomePage
